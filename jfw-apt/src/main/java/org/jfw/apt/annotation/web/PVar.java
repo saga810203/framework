a8683aaeb0a8803d646c89101c321da70a93bb10
pengjia <pengjia@isoftstone.com>
2016-06-15 15:03:09 +0800

    --

--- jfw-apt/src/main/java/org/jfw/apt/annotation/web/PVar.java
+++ jfw-apt/src/main/java/org/jfw/apt/annotation/web/PVar.java
@@ -6,12 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.jfw.apt.model.web.handlers.BuildParamHandler.BuildParameter;
-import org.jfw.apt.model.web.handlers.buildparam.PathVarHandler;
+import org.jfw.apt.model.web.handlers.buildparam.PVarHandler;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.SOURCE)
-public @interface PathVar {
+public @interface PVar {
 	boolean encoding() default false;
 	String pathAttribute() default "org.jfw.web.reqMacthUri_DYN";
-	Class<? extends BuildParameter> buildParamClass() default PathVarHandler.class;
+	Class<? extends BuildParameter> buildParamClass() default PVarHandler.class;
 }
--- jfw-apt/src/main/java/org/jfw/apt/model/web/handlers/buildparam/PVarHandler.java
+++ jfw-apt/src/main/java/org/jfw/apt/model/web/handlers/buildparam/PVarHandler.java
@@ -1,13 +1,13 @@
 package org.jfw.apt.model.web.handlers.buildparam;
 
-import org.jfw.apt.annotation.web.PathVar;
+import org.jfw.apt.Utils;
+import org.jfw.apt.annotation.web.PVar;
 import org.jfw.apt.exception.AptException;
 import org.jfw.apt.model.MethodParamEntry;
 import org.jfw.apt.model.web.RequestMappingCodeGenerator;
 import org.jfw.apt.model.web.handlers.BuildParamHandler;
 
-public class PathVarHandler extends BuildParamHandler.BuildParameter {
-
+public class PVarHandler extends BuildParamHandler.BuildParameter {
 	private int getIndexInPath(String name, String path) {
 		String pathL = "{" + name.trim() + "}";
 		String[] paths = path.split("/");
@@ -18,34 +18,51 @@ public class PathVarHandler extends BuildParamHandler.BuildParameter {
 		}
 		return -1;
 	}
-
+    public static final String TMP_VAR=PVarHandler.class.getName()+"_TMPVAR";
 	@Override
 	public void build(StringBuilder sb, MethodParamEntry mpe, RequestMappingCodeGenerator rmcg) throws AptException {
-		PathVar pv = (PathVar) mpe.getRef().getAnnotation(PathVar.class);
+		PVar pv = (PVar) mpe.getRef().getAnnotation(PVar.class);
 		if(pv==null) return;
 		
 		rmcg.readURI(sb,pv.pathAttribute());
-		String	val = mpe.getName().trim();
+		String val = mpe.getName().trim();
+
+		
 		String path = rmcg.getUri();
 		int pathIndex = getIndexInPath(val, path)-1;
 		if (pathIndex < 0)
 			throw new AptException(mpe.getRef(),"invalid annotation @PathVar ");
+		String tmpVar = (String)rmcg.getAttribute(TMP_VAR);
+		if(tmpVar==null){
+			sb.append("String ");
+			tmpVar= rmcg.getTempalteVariableName();
+			rmcg.setAttribute(TMP_VAR, tmpVar);
+		}
+		sb.append(tmpVar).append("=_uriArray[").append(pathIndex).append("].substring(1);\r\n");
 		
-		sb.append(mpe.getTypeName()).append(" ").append(mpe.getName()).append(" = ");
+		
+		sb.append(mpe.getTypeName()).append(" ").append(mpe.getName());
 		
 		String lTypeName = mpe.getTypeName();
+		
+		if(!Utils.isPrimitive(lTypeName)){
+			sb.append(" = null");
+		}
+		sb.append(";\r\n");
+		sb.append("if(").append(tmpVar).append("length()>0){").append(mpe.getName()).append("=");
+		
 		if (lTypeName.equals(int.class.getName())) {
-			sb.append("Integer.parseInt(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Integer.parseInt(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(Integer.class.getName())) {
-			sb.append("Integer.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Integer.valueOf(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(byte.class.getName())) {
-			sb.append("Byte.parseByte(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Byte.parseByte(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(Byte.class.getName())) {
-			sb.append("Byte.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Byte.valueOf(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(short.class.getName())) {
-			sb.append("Short.parseShort(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Short.parseShort(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(Short.class.getName())) {
-			sb.append("Short.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("Short.valueOf(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(long.class.getName())) {
 			sb.append("Long.parseLong(_uriArray[").append(pathIndex).append("]);\r\n");
 		} else if (lTypeName.equals(Long.class.getName())) {
@@ -59,21 +76,19 @@ public class PathVarHandler extends BuildParamHandler.BuildParameter {
 		} else if (lTypeName.equals(Float.class.getName())) {
 			sb.append("Float.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
 		} else if (lTypeName.equals(boolean.class.getName()) || lTypeName.equals(Boolean.class.getName())) {
-			sb.append("\"1\".equals(_uriArray[").append(pathIndex)
-					.append("])|| \"true\".equalsIgnoreCase(_uriArray[").append(pathIndex)
-					.append("])||\"yes\".equalsIgnoreCase(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("\"1\".equals(").append(tmpVar).append(")|| \"true\".equalsIgnoreCase(").append(tmpVar).append(")||\"yes\".equalsIgnoreCase(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(String.class.getName())) {
 			if (pv.encoding()) {
-				sb.append("java.net.URLDecoder.decode(_uriArray[").append(pathIndex).append("],\"UTF-8\");\r\n");
+				sb.append("java.net.URLDecoder.decode(").append(tmpVar).append("),\"UTF-8\");\r\n");
 			} else {
-				sb.append("_uriArray[").append(pathIndex).append("];\r\n");
+				sb.append(tmpVar).append(";\r\n");
 			}
 		} else if (lTypeName.equals(java.math.BigInteger.class.getName())) {
-			sb.append("java.math.BigInteger.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("java.math.BigInteger.valueOf(").append(tmpVar).append(");\r\n");
 		} else if (lTypeName.equals(java.math.BigDecimal.class.getName())) {
-			sb.append("java.math.BigDecimal.valueOf(_uriArray[").append(pathIndex).append("]);\r\n");
+			sb.append("java.math.BigDecimal.valueOf(").append(tmpVar).append(");\r\n");
 		} else {
-			throw new AptException(mpe.getRef(),"UnSupportedType on paramter with @PathVar");
+			throw new AptException(mpe.getRef(),"UnSupportedType on paramter with @PVar");
 		}
 	}
 
